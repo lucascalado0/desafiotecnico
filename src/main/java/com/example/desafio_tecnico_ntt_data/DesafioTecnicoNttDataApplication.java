e3102b88f1056a28c8cdb0ef65dda8615f1fee03
devlucascalado <devlucascalado@gmail.com>
2025-07-09 13:19:21 -0300

    adicionando dependencias

---
+++ src/main/java/com/example/desafio_tecnico_ntt_data/DesafioTecnicoNttDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_tecnico_ntt_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoNttDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnicoNttDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafio_tecnico_ntt_data/DesafioTecnicoNttDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_tecnico_ntt_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoNttDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
