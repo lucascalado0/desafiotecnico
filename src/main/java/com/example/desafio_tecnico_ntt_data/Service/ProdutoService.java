951755ad57a39b79618a9969804738d02d56de04
devlucascalado <devlucascalado@gmail.com>
2025-07-09 16:14:22 -0300

    construção camada service

--- src/main/java/com/example/desafio_tecnico_ntt_data/Service/ProdutoService.java
+++ src/main/java/com/example/desafio_tecnico_ntt_data/Service/ProdutoService.java
@@ -1,7 +1,39 @@
 package com.example.desafio_tecnico_ntt_data.Service;
 
+
+import com.example.desafio_tecnico_ntt_data.DTO.ProdutoDTO;
+import com.example.desafio_tecnico_ntt_data.Entity.Produto;
+import com.example.desafio_tecnico_ntt_data.Exception.RecursoNaoEncontradoException;
+import com.example.desafio_tecnico_ntt_data.Repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ProdutoService {
+
+    @Autowired
+    ProdutoRepository produtoRepository;
+
+    public Produto cadastrarProduto(ProdutoDTO produtoDTO){
+        Produto produto = produtoDTO.toEntity();
+
+        return produtoRepository.save(produto);
+    }
+
+    public List<ProdutoDTO> listarProdutos(){
+        return produtoRepository.findAll()
+                .stream()
+                .map(ProdutoDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    public Optional<ProdutoDTO> consultarProduto(Long id){
+        return Optional.ofNullable(produtoRepository.findById(id)
+                .map(ProdutoDTO::new)
+                .orElseThrow(() -> new RecursoNaoEncontradoException("Produto não encontrado com ID: " + id)));
+    }
 }
