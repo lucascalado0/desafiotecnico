23b9b8bc163a8871f789fde548a8aa00a767aa74
devlucascalado <devlucascalado@gmail.com>
2025-07-09 14:12:17 -0300

    add repository + service

---
+++ src/main/java/com/example/desafio_tecnico_ntt_data/repository/ProdutoRepository.java
@@ -0,0 +1,4 @@
+package com.example.desafio_tecnico_ntt_data.repository;
+
+public class ProdutoRepository {
+}
---
+++ src/main/java/com/example/desafio_tecnico_ntt_data/service/ProdutoService.java
@@ -0,0 +1,4 @@
+package com.example.desafio_tecnico_ntt_data.service;
+
+public class ProdutoService {
+}
