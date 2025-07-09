2ae76c3717b65c60a8e32531f3902d190b8c6816
devlucascalado <devlucascalado@gmail.com>
2025-07-09 16:14:07 -0300

    criação exceção

---
+++ src/main/java/com/example/desafio_tecnico_ntt_data/Exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.example.desafio_tecnico_ntt_data.Exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+    public RecursoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
