450697b0f58d6f07ff59c0fcb530d865c0dd8a1d
devlucascalado <devlucascalado@gmail.com>
2025-07-09 14:14:05 -0300

    add repository + service

--- src/main/java/com/example/desafio_tecnico_ntt_data/repository/ProdutoRepository.java
+++ src/main/java/com/example/desafio_tecnico_ntt_data/repository/ProdutoRepository.java
@@ -1,4 +1,10 @@
 package com.example.desafio_tecnico_ntt_data.repository;
 
-public class ProdutoRepository {
+import com.example.desafio_tecnico_ntt_data.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
 }
--- src/main/java/com/example/desafio_tecnico_ntt_data/service/ProdutoService.java
+++ src/main/java/com/example/desafio_tecnico_ntt_data/service/ProdutoService.java
@@ -1,4 +1,7 @@
 package com.example.desafio_tecnico_ntt_data.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProdutoService {
 }
