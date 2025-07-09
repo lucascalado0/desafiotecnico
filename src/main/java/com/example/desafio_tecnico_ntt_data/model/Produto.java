36c52e95e0b68731a96d3958800c90f0d1e786cf
devlucascalado <devlucascalado@gmail.com>
2025-07-09 14:01:09 -0300

    classe produto

---
+++ src/main/java/com/example/desafio_tecnico_ntt_data/model/Produto.java
@@ -0,0 +1,35 @@
+package com.example.desafio_tecnico_ntt_data.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Data
+@Entity(name = "produto")
+@Table(name = "tb_produto")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_produto")
+    private long idProduto;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 200)
+    private String descricao;
+
+    @Column(nullable = false)
+    private double preco;
+
+    public Produto(String nome, String descricao, double preco){
+        this.nome = nome;
+        this.descricao = descricao;
+        if(preco < 0) {
+            throw new IllegalArgumentException("O preço não pode ser negativo.");
+        } else {
+            this.preco = preco;
+        }
+    }
+}
