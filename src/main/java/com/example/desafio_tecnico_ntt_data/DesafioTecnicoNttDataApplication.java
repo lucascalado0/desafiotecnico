9807256eb193f6484b6d2f49037415147898c089
devlucascalado <devlucascalado@gmail.com>
2025-07-13 13:19:27 -0300

    habilitando api no eureka

--- src/main/java/com/example/desafio_tecnico_ntt_data/DesafioTecnicoNttDataApplication.java
+++ src/main/java/com/example/desafio_tecnico_ntt_data/DesafioTecnicoNttDataApplication.java
@@ -3,6 +3,9 @@ package com.example.desafio_tecnico_ntt_data;
 import com.example.desafio_tecnico_ntt_data.Entity.Produto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DesafioTecnicoNttDataApplication {
@@ -14,6 +17,15 @@ public class DesafioTecnicoNttDataApplication {
 		Produto produto = new Produto("Notebook", "Notebook Dell Inspiron 15", 3500.00);
 		System.out.println(produto);
 
+	}
+
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer(){
+		return factory -> {
+			int port = 8100 + (int) (Math.random() * 100);
+
+			factory.setPort(port);
+		};
 	}
 
 }
